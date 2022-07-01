2841594e2c45211393856f760168670b8ba7493f
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-01 21:34:11 +0900

    프로젝트 생성

---
+++ src/test/java/com/project/dogfaw/DogfawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dogfaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogfawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
