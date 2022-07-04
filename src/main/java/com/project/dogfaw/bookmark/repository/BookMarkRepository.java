4ed09b1b786f59343917bd21f153ea814d5a484e
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-04 09:25:36 +0900

    북마크, 참여신청 수정중

--- src/main/java/com/project/dogfaw/bookmark/repository/BookMarkRepository.java
+++ src/main/java/com/project/dogfaw/bookmark/repository/BookMarkRepository.java
@@ -1,7 +1,7 @@
 package com.project.dogfaw.bookmark.repository;
 
 import com.project.dogfaw.bookmark.model.BookMark;
-import com.project.dogfaw.post.Post;
+import com.project.dogfaw.post.model.Post;
 import com.project.dogfaw.user.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
