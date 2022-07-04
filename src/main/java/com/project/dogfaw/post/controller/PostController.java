8bc69b67af217c9487572597b4597663e9e17bd1
dasole123 <dasole1234@gmail.com>
2022-07-04 18:03:34 +0900

    키워드 - postRequestDto, postResponseDto 업데이트, post controller 수정중

--- src/main/java/com/project/dogfaw/post/controller/PostController.java
+++ src/main/java/com/project/dogfaw/post/controller/PostController.java
@@ -1,15 +1,34 @@
 package com.project.dogfaw.post.controller;
 
 
+import com.project.dogfaw.post.dto.PostRequestDto;
+import com.project.dogfaw.post.dto.PostResponseDto;
+import com.project.dogfaw.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+
 public class PostController {
 
-    @PostMappping
+    private final PostService postService;
 
+    //post 전체조회
+//    @GetMapping("/api/allpost")
+//    public PostResponseDto postPosts(@RequestBody PostRequestDto postRequestDto, @RequestHeader("Authorization") String authorization) {
+//        return postService.postPost(postRequestDto, authorization);
+//    }
+//    //post 생성
+//    @PutMapping("/api/post")
+//    public List<PostResponseDto> getPosts(){
+//        return postService.getPosts();
+//    }
+//
+//    //post 상세조회
+//    @GetMapping("/")
+
+    //post 수정
+    //post 삭제
 
 }
