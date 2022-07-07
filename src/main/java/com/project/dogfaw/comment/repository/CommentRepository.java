28a3fd8815722e10b627a7be663ac155ebcc21fa
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-07 23:02:25 +0900

    서버 첫 배포

--- src/main/java/com/project/dogfaw/comment/repository/CommentRepository.java
+++ src/main/java/com/project/dogfaw/comment/repository/CommentRepository.java
@@ -1,14 +1,14 @@
-package com.project.dogfaw.comment.repository;
-
-import com.project.dogfaw.post.model.Post;
-import com.project.dogfaw.user.model.User;
-import jdk.javadoc.internal.doclets.toolkit.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Content, Long> {
-    List<Content> findAllByPost(Post postId);
-
-    List<Content> findAllByPostAndUser(Post post, User user);
-}
+//package com.project.dogfaw.comment.repository;
+//
+//import com.project.dogfaw.post.model.Post;
+//import com.project.dogfaw.user.model.User;
+//import jdk.javadoc.internal.doclets.toolkit.Content;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface CommentRepository extends JpaRepository<Content, Long> {
+//    List<Content> findAllByPost(Post postId);
+//
+//    List<Content> findAllByPostAndUser(Post post, User user);
+//}
