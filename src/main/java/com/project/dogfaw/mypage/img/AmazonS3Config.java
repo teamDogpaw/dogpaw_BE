8e9a4ca4c60a1366ded8ad16f7ab20b5de0da08b
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-08 18:56:31 +0900

    Post db 테이블에서 online 컬럼 boolean -> String 으로 전환, 메인페이지 전체 조회 findAllByModifiedAtDesc -> findAllByCreatedAtDesc 으로 변경(), 디테일페이지 조회에서 리스폰스 값에 로그인한 유저의 apply Status(true or false) 추가, S3 프로필 이미지는 작업중...(bean 주입 문제 발생)

---
+++ src/main/java/com/project/dogfaw/mypage/img/AmazonS3Config.java
@@ -0,0 +1,2 @@
+package com.project.dogfaw.mypage.img;public class AmazonS3Config {
+}
---
+++ src/main/java/com/project/dogfaw/mypage/img/S3Uploader.java
@@ -0,0 +1,88 @@
+package com.project.dogfaw.mypage.img;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.project.dogfaw.comment.service.CommentService;
+import com.project.dogfaw.common.exception.CustomException;
+import com.project.dogfaw.common.exception.ErrorCode;
+import com.project.dogfaw.mypage.dto.MypageRequestDto;
+import com.project.dogfaw.user.model.Stack;
+import com.project.dogfaw.user.model.User;
+import com.project.dogfaw.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class S3Uploader {
+
+    private final UserRepository userRepository;
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket; //S3 버킷 이름
+
+    public String upload(MultipartFile multipartFile, String dirNam, User user, MypageRequestDto requestDto) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+
+        return upload(uploadFile, dirNam, user, requestDto);
+    }
+
+    @Transactional
+    public String upload(File uploadFile, String dirName, User user, MypageRequestDto requestDto) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        //닉네임 중복검사
+        String nickname = requestDto.getNickName();
+        if (userRepository.existsByNickname(nickname)) {
+            throw new CustomException(ErrorCode.SIGNUP_NICKNAME_DUPLICATE_CHECK);
+        } else {
+            user.setNickname(requestDto.getNickName());
+            user.setStacks(requestDto.getStacks());
+            user.setProfileImg(uploadImageUrl);
+            return uploadImageUrl;
+        }
+    }
+
+    //S3로 업로드
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    //로컬에 저장된 이미지 지우기
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+}
