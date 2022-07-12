3a2e3a9288aee191b6cce558b03e98349b051b2e
dasole123 <dasole1234@gmail.com>
2022-07-12 13:36:13 +0900

    키워드 - comment dto, comment model에서 comment -> content 이름 변경, commentReply(대댓글)작업중 (아직구현X)

--- src/main/java/com/project/dogfaw/comment/dto/CmtReplyResponseDto.java
+++ src/main/java/com/project/dogfaw/comment/dto/CmtReplyResponseDto.java
@@ -1,27 +1,17 @@
 package com.project.dogfaw.comment.dto;
 
 import com.project.dogfaw.comment.model.Comment;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
-
-@Getter
-@NoArgsConstructor
-public class CommentResponseDto {
-
-    private Long commentId;
+public class CmtReplyResponseDto {
+    private Long commentReplyId;
     private String content;
     private String modifiedAt;
     private Long userId;
     private String nickname;
     private String profileImg;
 
-    public CommentResponseDto(Comment comment) {
-        this.commentId = comment.getId();
+    public CmtReplyResponseDto(Comment comment) {
+        this.commentReplyId = comment.getId();
         this.content = comment.getContent();
         this.userId = comment.getUser().getId();
         this.nickname = comment.getUser().getNickname();
--- src/main/java/com/project/dogfaw/comment/dto/CommentRequestDto.java
+++ src/main/java/com/project/dogfaw/comment/dto/CommentRequestDto.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 
 public class CommentRequestDto {
 
-    private String comment;
+    private String content;
 
 
 }
