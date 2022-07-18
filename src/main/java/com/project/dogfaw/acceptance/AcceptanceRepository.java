f5234dd9527283436a9d4a0a541b25a48a94e468
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-18 14:00:03 +0900

    Post 상세페이지조회 GET - reponse 값에 userStatus 추가(작성자 = writer,수락된유저 = participant, 신청자 = applicant, 신청안한사람 = member)

--- src/main/java/com/project/dogfaw/acceptance/AcceptanceRepository.java
+++ src/main/java/com/project/dogfaw/acceptance/AcceptanceRepository.java
@@ -1,5 +1,6 @@
 package com.project.dogfaw.acceptance;
 
+import com.project.dogfaw.post.model.Post;
 import com.project.dogfaw.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import java.util.List;
 public interface AcceptanceRepository extends JpaRepository<Acceptance, Long> {
 
     List<Acceptance> findAllByUser(User user);
+    Boolean existsByUserAndPost(User user, Post post);
 
 }
--- src/main/java/com/project/dogfaw/post/model/UserStatus.java
+++ src/main/java/com/project/dogfaw/post/model/UserStatus.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 public enum UserStatus {
 
     USER_STATUS_WRITER("writer"), //작성자
-    USER_STATUS_MEMBER("member"), //일반유저
-    USER_STATUS_NORMAL("memr"), //신청완료
-    USER_STATUS_NOTYET("mer"); //지원안한사람
+    USER_STATUS_PARTICIPANT("participant"), //수락된사람
+    USER_STATUS_APPLICANT("applicant"), //신청완료
+    USER_STATUS_MEMBER("MEMBER"); //지원안한사람
 
     private String userStatus;
 }
