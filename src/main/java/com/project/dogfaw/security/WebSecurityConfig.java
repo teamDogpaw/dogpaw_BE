2fc60f31dc6fe222934a0cd5a1f1253d9f23a046
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-24 04:18:29 +0900

    서버에러 - DB테이블이 꼬여서 에러 발생했었음. 처리완료

--- src/main/java/com/project/dogfaw/security/WebSecurityConfig.java
+++ src/main/java/com/project/dogfaw/security/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
+
 
         http.cors().configurationSource(corsConfigurationSource());
         http.csrf().disable().sessionManagement()
