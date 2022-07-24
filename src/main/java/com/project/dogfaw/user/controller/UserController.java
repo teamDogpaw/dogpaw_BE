7068b35e8cc713d6f5c432f3846800fea2541173
질문KoSeungyu < koeul123@gmail.com>
2022-07-24 15:32:56 +0900

    oAuth 주소변경

--- src/main/java/com/project/dogfaw/user/controller/UserController.java
+++ src/main/java/com/project/dogfaw/user/controller/UserController.java
@@ -87,7 +87,7 @@ public class UserController {
     public void kakaoLogin(@RequestParam String code, HttpServletResponse response) throws IOException {
         KakaoUserInfo kakaoUserInfo = kakaoUserService.kakaoLogin(code);
         String accesstoken = userService.SignupUserCheck(kakaoUserInfo.getKakaoId());
-        String url = "https://d2yxbwsc3za48s.cloudfront.net/?token=" + accesstoken;
+        String url = "http://localhost:3030/?token=" + accesstoken;
         response.sendRedirect(url);
     }
 
