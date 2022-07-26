ba6072ff900e8aa63b745542a404a340c17fa2c0
질문KoSeungyu < koeul123@gmail.com>
2022-07-26 17:03:42 +0900

    oAuth 수정중

--- src/main/java/com/project/dogfaw/common/CommonService.java
+++ src/main/java/com/project/dogfaw/common/CommonService.java
@@ -4,6 +4,7 @@ import com.project.dogfaw.security.UserDetailsImpl;
 import com.project.dogfaw.user.model.User;
 import com.project.dogfaw.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,13 +12,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CommonService {
     private final UserRepository userRepository;
 
     public User getUser() {
+        log.info("===========================1-1고승유=====================================================");
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
         String username = principal.getUser().getUsername();
+        log.info("===========================1-2고승유=====================================================");
+
         return userRepository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("존재하지 않는 유저입니다")
         );
