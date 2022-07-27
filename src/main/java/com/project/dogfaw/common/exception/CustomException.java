112520a84b1619a925047a695de427b856f88dbf
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-27 15:14:05 +0900

    충돌 수정중

--- src/main/java/com/project/dogfaw/common/exception/CustomException.java
+++ src/main/java/com/project/dogfaw/common/exception/CustomException.java
@@ -4,5 +4,10 @@ import lombok.Getter;
 
 @Getter
 public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
 
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
 }
