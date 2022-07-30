418bb65fbb39c2ccde4ee500b0e80722ace6f922
박건영(Parkgunyoung) <104492837+0AndWild@users.noreply.github.com>
2022-07-31 07:38:38 +0900

    대댓글에서 변경된 유저 프로필 닉네임이 안불러와지는 것 수정

--- src/main/java/com/project/dogfaw/comment/dto/CommentResponseDto.java
+++ src/main/java/com/project/dogfaw/comment/dto/CommentResponseDto.java
@@ -2,9 +2,11 @@ package com.project.dogfaw.comment.dto;
 
 import com.project.dogfaw.comment.model.Comment;
 import com.project.dogfaw.comment.model.CommentReply;
+import com.project.dogfaw.user.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +23,10 @@ public class CommentResponseDto {
     private String profileImg;
     private List<CommentReply> commentReplyList;
 
+    private Long comment;
+
+    private Long user;
+
     public CommentResponseDto(Comment comment) {
         this.commentId = comment.getId();
         this.content = comment.getContent();
@@ -28,10 +34,22 @@ public class CommentResponseDto {
         this.nickname = comment.getUser().getNickname();
         this.profileImg = comment.getUser().getProfileImg();
         this.modifiedAt = comment.getModifiedDate().toString();
-        this.commentReplyList = comment.getCommentReplyList();
+        this.commentReplyList = CommentReplyList(comment);
     }
 
 
-
+    public List<CommentReply> CommentReplyList(Comment comment){
+        List<CommentReply> commentReplylists = comment.getCommentReplyList();
+        List<CommentReply> commentReplies = new ArrayList<>();
+        for(CommentReply commentReply :commentReplylists ){
+            this.comment = comment.getId();
+            this.content = commentReply.getContent();
+            this.nickname =commentReply.getUser().getNickname();
+            this.profileImg = commentReply.getUser().getProfileImg();
+            this.user = commentReply.getUser().getId();
+            commentReplies.add(commentReply);
+        }
+        return commentReplies;
+    }
 
 }
